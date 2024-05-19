07b2688d4724937948ef576d8f04ed0a77b9d25d
Utku Karatas <utku.karatas3@gmail.com>
2024-05-19 17:41:23 +0300

    ucuncu commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:jamapp/widgets/custom_card.dart';
+import 'package:jamapp/widgets/country_image.dart';
+import 'package:jamapp/widgets/splash_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -22,21 +23,58 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           title: Text("HOME"),
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, top: 5.0),
-              child: Text(
-                "İtalya 🇮🇹",
-                style: GoogleFonts.sairaCondensed(fontSize: 25),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              CountryImage(
+                thumbnailUrl: "assets/resimler/italya.jpg",
               ),
-            ),
-            RecipeCard(
-                title: "Test",
-                cookTime: "15",
-                rating: "10/10",
-                thumbnailUrl: "")
-          ],
+              SplashCard(
+                ulkeAdi: "İtalya",
+                ulkeEmoji: "🇮🇹",
+                yemek1:
+                    "Pizza: İtalya'nın dünyaca ünlü yemeği. En klasik hali olan Margherita, domates, mozzarella peyniri ve taze fesleğen ile yapılır.",
+                yemek2:
+                    "Makarna: Çeşitli makarna türleri ve sosları ile İtalya mutfağının temel taşlarından biridir.",
+                yemek3:
+                    "Risotto: Kremsi bir yapıya sahip, genellikle sebze, et veya deniz ürünleri ile zenginleştirilmiş İtalyan pirinç yemeğidir",
+                yemek4:
+                    "Tiramisu: İtalyan mutfağının en ünlü tatlılarından biridir. Mascarpone peyniri, kahve, kakao ve bisküvi katmanlarından oluşur.",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CountryImage(thumbnailUrl: "assets/resimler/avusturya.png"),
+              SplashCard(
+                ulkeAdi: "Avusturya",
+                ulkeEmoji: "🇦🇹",
+                yemek1:
+                    "Wiener Schnitzel: Avusturya'nın en ünlü yemeği olan Wiener Schnitzel, ince dövülmüş dana etinin pane edilerek kızartılmasıyla hazırlanır. Genellikle limon dilimi ve patates salatası ile servis edilir.",
+                yemek2:
+                    "Tafelspitz: Bu yemek, haşlanmış dana eti ve çeşitli sebzelerden oluşur. Genellikle elma yahnisi ve frenk turpu ile servis edilir.",
+                yemek3:
+                    "Apfelstrudel: İnce yufkadan yapılmış elmalı strudel, tarçın, şeker ve bazen kuru üzümle doldurulur. Üzerine pudra şekeri serpilir ve genellikle vanilyalı dondurma veya krem şanti ile servis edilir.",
+                yemek4:
+                    "Gulasch: Macar mutfağından etkilenen bu yemek, et, soğan, kırmızı biber ve diğer baharatlarla yapılan doyurucu bir yahni türüdür.",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CountryImage(thumbnailUrl: "assets/resimler/fransa.jpg"),
+              SplashCard(
+                ulkeAdi: "Fransa",
+                ulkeEmoji: "🇫🇷",
+                yemek1:
+                    "Croissant: Fransız kahvaltılarının vazgeçilmezi olan bu tereyağlı ve kat kat hamurdan yapılan kruvasan, çıtır ve lezzetli bir hamur işidir.",
+                yemek2:
+                    "Coq au Vin: Tavuk etinin kırmızı şarap, mantar, soğan, sarımsak ve domuz pastırması ile pişirilmesiyle yapılan bu yemek, Fransız mutfağının klasiklerinden biridir.",
+                yemek3:
+                    "Bouillabaisse: Provence bölgesine özgü olan bu balık çorbası, çeşitli balıklar, deniz ürünleri, safran ve baharatlarla yapılır. Genellikle rouille sosu ve kruton ile servis edilir.",
+                yemek4:
+                    "Ratatouille: Patlıcan, kabak, biber, domates, soğan ve sarımsak gibi sebzelerle yapılan bir tür sebze güveçtir. Özellikle Provence mutfağının bir parçasıdır.",
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -8,6 +9,7 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<ProfileScreen> {
+  final user = FirebaseAuth.instance.currentUser!;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,8 +19,28 @@ class _HomeScreenState extends State<ProfileScreen> {
       ),
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text("PROFILE"),
+        appBar: AppBar(title: Text("PROFILE"), actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+          ),
+        ]),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                user.email!,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              Text(
+                "olarak giriş yapıldı",
+                style: TextStyle(fontSize: 20),
+              )
+            ],
+          ),
         ),
       ),
     );
